6aad4843ae140a2c4563656e8f7aa4296d0a3a0d
saiful <saiful0687@gmail.com>
2024-02-02 17:41:15 -0500

    crud created

---
+++ lib/dart_app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class DartApp extends StatelessWidget {
+  const DartApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:crud_app/dart_app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const DartApp());
+}
